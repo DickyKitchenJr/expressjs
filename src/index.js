aa8c280b8ae43f38e521ef05fa04e3a9ce4d3d2e
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-04 14:05:04 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ const app = express();
 
 const PORT = 3001;
 
+app.use(express.json())
+
+const authors = [{ author: "Dicky Kitchen Jr", book: "Prey/Pray" }];
+
+app.get('/', (req, res) =>{
+    res.status(200).send(authors)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    authors.push(req.body)
+    res.sendStatus(201)
+})
+
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
