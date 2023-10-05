c25a27e988f478b2dac14bac2e8f168aa9bc2166
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-05 14:10:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const app = express();
 
 const PORT = 3001;
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded());
 
 const authors = [{ author: "Dicky Kitchen Jr", book: "Prey/Pray" }];
 
