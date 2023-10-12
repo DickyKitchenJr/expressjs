66d732655fb3f631e74b19069e0326aab2f72114
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-12 11:35:56 -0400

    Update index.js
    - added a GET request with a parameter
    
    - formatting auto-changed the rest of the file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -7,18 +7,32 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded());
 
-const authors = [{ author: "Dicky Kitchen Jr", book: "Prey/Pray" }];
+const authors = [
+  { author: "Dicky Kitchen Jr", book: "Prey/Pray", id: 1 },
+  { author: "Stephen Mills", book: "Vampire", id: 2 },
+  { author: "Gregg Hurwitz", book: "Orphan X", id: 3 },
+];
 
-app.get('/', (req, res) =>{
-    res.status(200).send(authors)
-})
+app.get("/", (req, res) => {
+  res.status(200).send(authors);
+});
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    authors.push(req.body)
-    res.sendStatus(201)
-})
+app.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const authorRequested = authors.find((authorSearched) => authorSearched.id === parseInt(id));
+    if(authorRequested){
+        res.status(200).send(authorRequested);
+    } else {
+        res.status(404).send({message: "author with matching ID not found"})
+    }
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  authors.push(req.body);
+  res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
-})
+  console.log(`Running on port ${PORT}`);
+});
