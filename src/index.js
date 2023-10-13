2a937aa9523ba4b1f3b2fc9861f8da284914cd83
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-13 11:27:54 -0400

    Update index.js
    implemented cookie-parser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const authorsRoutes = require('./routes/authors');
 const characterRoutes = require('./routes/comicCharacters');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 const PORT = 3001;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
