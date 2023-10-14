e669f0cbb0e6dd3d6a3d0dad6784c20c4e07ac6e
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-14 18:36:20 -0400

    set up fake auth
    set up fake authorizations to experiment with requiring some form of login to access the other sections

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const authorsRoutes = require('./routes/authors');
 const characterRoutes = require('./routes/comicCharacters');
 const cookieParser = require("cookie-parser");
 const session = require('express-session');
+const authRoute = require('./routes/auth');
 
 const app = express();
 
@@ -17,9 +18,16 @@ app.use(session({
   saveUninitialized: false
 }))
 
-app.use('/api/authors', authorsRoutes);
+app.use("/api/auth", authRoute);
 
+app.use((req, res, next) =>{
+  if(req.session.user) next();
+  else res.status(401).send('not logged in')
+})
+
+app.use('/api/authors', authorsRoutes);
 app.use('/api/characters', characterRoutes)
+
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
