2bc69d7af28bf33c42cce0fc7e1a5fe36c3fbdba
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-16 13:59:09 -0400

    Create Users.js
    created schema to add users

---
+++ src/database/schemas/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+module.exports = mongoose.model("user", UsersSchema);
