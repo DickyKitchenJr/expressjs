fc14ff09c1b330cf67daade7a4cb13e306b2daea
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-16 13:59:54 -0400

    Update auth.js
    added a post method for added new user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const Users = require('../database/schemas/Users');
+const { hashPassword } = require('../utilities/helpers');
 
 const router = Router();
 
@@ -17,5 +19,18 @@ router.post("/login", (req, res) => {
     res.status(401).send("Username and password required");
   }
 });
+
+router.post('/adduser', async (req, res) => {
+  const { name } = req.body;
+  const userDB = await Users.findOne({name});
+  if(userDB){
+    res.status(400).send({ message: "User already exist" });
+  } else {
+    const password = hashPassword(req.body.password);
+    console.log(password);
+    const newUser = await Users.create({ name, password });
+    res.status(201).send({ message: "User added" });
+  }
+})
 
 module.exports = router;
