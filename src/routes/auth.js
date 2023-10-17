a525e15d38dfcdbb716f396487172157dc1f9044
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-17 11:20:53 -0400

    Update auth.js
    added login verification to auth.js with cookie using database to check if user exist and has valid password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,23 +1,22 @@
 const { Router } = require("express");
 const Users = require('../database/schemas/Users');
-const { hashPassword } = require('../utilities/helpers');
+const { hashPassword, comparePassword } = require('../utilities/helpers');
 
 const router = Router();
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (username && password) {
-    if (req.session.user) {
-      res.send(req.session.user);
-    } else {
-      req.session.user = {
-        username,
-      };
-      res.send(req.session);
-    }
-  } else {
-    res.status(401).send("Username and password required");
-  }
+router.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  if(!name || !password) return res.status(400).send({ message: "Name and Password required"});
+  const userDB = await Users.findOne({name});
+  console.log(userDB);
+  if(!userDB) return res.send(401);
+  const isValid = comparePassword(password, userDB.password);
+   if(isValid){
+    req.session.user = userDB;
+    res.send(200);  
+   } else {
+    res.send(400)
+   }
 });
 
 router.post('/adduser', async (req, res) => {
