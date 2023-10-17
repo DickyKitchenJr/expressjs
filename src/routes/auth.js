4355122be54651751708af0673bc34f8e8d9d0f4
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-17 15:13:56 -0400

    Update auth.js
    commented out previous logic for handling login and refactored it using passport (while putting the actual logic in the strategies folder under local.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,23 +1,29 @@
 const { Router } = require("express");
+const passport = require('passport');
 const Users = require('../database/schemas/Users');
 const { hashPassword, comparePassword } = require('../utilities/helpers');
 
 const router = Router();
 
-router.post("/login", async (req, res) => {
-  const { name, password } = req.body;
-  if(!name || !password) return res.status(400).send({ message: "Name and Password required"});
-  const userDB = await Users.findOne({name});
-  console.log(userDB);
-  if(!userDB) return res.send(401);
-  const isValid = comparePassword(password, userDB.password);
-   if(isValid){
-    req.session.user = userDB;
-    res.send(200);  
-   } else {
-    res.send(400)
-   }
-});
+// router.post("/login", async (req, res) => {
+//   const { name, password } = req.body;
+//   if(!name || !password) return res.status(400).send({ message: "Name and Password required"});
+//   const userDB = await Users.findOne({name});
+//   console.log(userDB);
+//   if(!userDB) return res.send(401);
+//   const isValid = comparePassword(password, userDB.password);
+//    if(isValid){
+//     req.session.user = userDB;
+//     res.send(200);  
+//    } else {
+//     res.send(400)
+//    }
+// });
+
+router.post('/login', passport.authenticate('local'), (req, res) =>{
+  console.log('Logged In')
+  res.send(200);
+})
 
 router.post('/adduser', async (req, res) => {
   const { name } = req.body;
